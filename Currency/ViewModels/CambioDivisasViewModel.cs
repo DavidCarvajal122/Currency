ddc37a18737a851be48780d3cb003f581fdfbca1
DavidCarvajal122 <david.carvajal.delrosario@udla.edu.ec>
2025-06-11 07:39:30 -0500

    Agregar archivos de proyecto.

---
+++ Currency/ViewModels/CambioDivisasViewModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Currency.ViewModels
+{
+    internal class CambioDivisasViewModel : INotifyPropertyChanged
+    {
+        private string _valorUSD;
+        private string _valorEUR;
+
+        public string ValorUSD
+        {
+            get => _valorUSD;
+            set {
+                if (_valorUSD != value)
+                {
+                    _valorUSD = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public void CambiarDeDolaresAEuros { 
+
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string name = "") =>
+
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        
+    }
+}
